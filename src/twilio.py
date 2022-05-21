2ba8bdc8cc151cbfefa5ed17cb54cffc8ac6d55a
Paul Evans <pauljevans927@gmail.com>
2022-05-21 18:59:46 -0400

    fixed sid issue

--- src/twilio.py
+++ src/twilio.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 
 PHONE_NUMBER = "+18044947762"
+APP_SID="MG190f52ced38f400b2ee059f72dacdff6"
 
 load_dotenv()
 TOKEN = str(os.getenv('TOKEN'))
@@ -10,5 +11,5 @@ SID = str(os.getenv('SID'))
 
 def sendMessage(body, number):
     client = Client(SID, TOKEN)
-    client.messages.create(to=number, from_=PHONE_NUMBER, body=body)
+    client.messages.create(to=number, from_=PHONE_NUMBER, body=body, messaging_service_sid=APP_SID)
     

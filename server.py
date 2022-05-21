aa67c4b157461974a67ede2eef7718ceb6d9226c
Malcolm Krolick <malcolm.krolick@gmail.com>
2022-05-21 12:22:43 -0400

    Added Old Code
    crud needs to be tailored
    
    basic server.py set up

---
+++ server.py
@@ -0,0 +1,24 @@
+# python imports
+import os
+import uuid
+
+# external imports 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+#postgres server
+from postgres.crud import Backend_Interface
+
+
+# startup
+app = FastAPI()
+
+####### ROUTES [GENERAL] #######
+
+@app.get("/ping", status_code=200)
+async def ping():
+    return {"message": "pong"} 
+
+@app.get("/home", status_code=200)
+async def ping():
+    return {"message": "home"}

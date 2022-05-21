40e3abda97edec671451d52f2bc4c46e4e0da48a
Paul Evans <pauljevans927@gmail.com>
2022-05-21 14:05:37 -0400

    cleaned the twilio function

--- src/twilio.py
+++ src/twilio.py
@@ -1,6 +1,5 @@
 import os
 from twilio.rest import Client
-from datetime import datetime
 from dotenv import load_dotenv
 
 PHONE_NUMBER = "+18044947762"
@@ -10,11 +9,5 @@ TOKEN = str(os.getenv('TOKEN'))
 SID = str(os.getenv('SID'))
 
 def sendMessages(body, number):
-
     client = Client(SID, TOKEN)
-
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-
     client.messages.create(to=number, from_=PHONE_NUMBER, body=body)
-    client.messages.create(to=number, from_=PHONE_NUMBER, body=body) 

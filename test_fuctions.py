3666eee00c0246717cb8b2e56c7e9acc39cfd52c
Malcolm Krolick <malcolm.krolick@gmail.com>
2022-05-21 21:39:26 -0400

    Changed Models and edited add user route
    Changed AddUserRequest to only take oauth and phone number
    
    edited route to use Pydantic model

---
+++ test_fuctions.py
@@ -0,0 +1,16 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+Oauth = os.getenv('OAUTH_TOKEN')
+
+phone_number = "+19143348636"
+
+json = {
+    "phone_number": ,
+}
+
+
+requests.post()

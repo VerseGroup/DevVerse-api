5e26021fe0f5ca9dadb595cfb36df60812cb20e4
Malcolm Krolick <malcolm.krolick@gmail.com>
2022-05-21 23:55:50 -0400

    Cleaning Create Tables

--- create_tables.py
+++ create_tables.py
@@ -1,13 +1,7 @@
-from code import interact
-import os
-import psycopg2
 from src.postgres.crud import Backend_Interface
 
-DATABASE_URL = os.environ['DATABASE_URL']
 
 if __name__ == "__main__":
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
     interfaces = Backend_Interface()
     interfaces.create_user_table()
     interfaces.create_todo_table()

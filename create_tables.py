c4366c9ccd857708a911633941683019eb0c8e12
Malcolm Krolick <malcolm.krolick@gmail.com>
2022-05-22 06:47:53 -0400

    Commented Out unnecessary code

--- create_tables.py
+++ create_tables.py
@@ -4,7 +4,8 @@ from src.postgres.crud import Backend_Interface
 if __name__ == "__main__":
     interfaces = Backend_Interface()
     interfaces.create_user_table()
-    interfaces.create_todo_table()
-    interfaces.create_task_table()
+    #interfaces.create_todo_table()
+    #interfaces.create_task_table()
+    interfaces.create_idea_table()
     interfaces.conn.close()
 
